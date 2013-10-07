c35edfcf59de4e0d73b7e65bf44868350624b545
Luke van der Hoeven <hungerandthirst@gmail.com>
2013-10-07 12:14:51 -0400

    commit cleaned up TFSClient
    accidentally had keys

---
+++ lib/tfs_graph/tfs_client.rb
@@ -0,0 +1,29 @@
+# Wraps TFS OData domain knowledge
+require 'tfs'
+
+module TFSGraph
+  module TFSClient
+    InvalidConfig = Class.new(RuntimeError)
+
+    REQUIRED_KEYS = [:endpoint, :username, :password]
+
+    # Requires a has
+    def setup(settings)
+      raise InvalidConfig unless REQUIRED_KEYS.all? {|key| settings.keys.include? key }
+
+      TFS.configure do |c|
+        c.endpoint = settings[:endpoint]
+        c.username = settings[:username]
+        c.password = settings[:password]
+        c.namespace = settings[:namespace] || "TFS"
+      end
+    end
+
+    def tfs
+      @tfs ||= begin
+        setup
+        TFS.client
+      end
+    end
+  end
+end
