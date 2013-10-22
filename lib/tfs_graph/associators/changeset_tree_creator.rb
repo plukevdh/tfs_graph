54b5d5dbe94b1e80930215c44b3745a338e8427d
Luke van der Hoeven <hungerandthirst@gmail.com>
2013-10-22 15:17:22 -0400

    get changesets for calc from branch
    ensures we have all currently associated changesets

--- lib/tfs_graph/associators/changeset_tree_creator.rb
+++ lib/tfs_graph/associators/changeset_tree_creator.rb
@@ -1,9 +1,9 @@
 module TFSGraph
 	class ChangesetTreeCreator
 		class << self
-			def to_tree(changesets, branch)
+			def to_tree(branch)
+				changesets = branch.changesets
 				changesets.each.with_index do |changeset, i|
-					# get the current changeset's parent (the branch if first)
 					parent = (i == 0) ? branch : changesets[i-1]
 
 					if Changeset.find parent.id
