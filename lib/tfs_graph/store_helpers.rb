348009fb3bdd070b2b6af32c02f26ae7bd701626
Luke van der Hoeven <hungerandthirst@gmail.com>
2013-10-28 16:07:53 -0400

    get last updated time from last cached changeset

--- lib/tfs_graph/store_helpers.rb
+++ lib/tfs_graph/store_helpers.rb
@@ -4,12 +4,25 @@ module TFSGraph
   module StoreHelpers
     UPDATED_KEY = "LAST_UPDATED_ON"
 
-    def mark_as_updated
-      Related.redis.set UPDATED_KEY, Time.now.utc
+    def mark_as_updated(time=nil)
+      time ||= Time.now
+      Related.redis.set UPDATED_KEY, time.utc
     end
 
     def last_updated_on
-      Time.parse(Related.redis.get(UPDATED_KEY)).localtime
+      date = Related.redis.get(UPDATED_KEY)
+      return last_updated_from_project unless date
+
+      Time.parse(date).localtime
+    end
+
+    def last_updated_from_project
+      last_changes = ProjectStore.all_cached.map {|p| p.last_change }.flatten.compact
+      last_time = last_changes.max {|a,b| a.created <=> b.created }.created
+
+      # cache if we're looking up for the first time
+      mark_as_updated last_time.dup
+      last_time
     end
   end
 end
