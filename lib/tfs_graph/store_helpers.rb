8187f842549eee0e34b5358ed304490df7549e28
Luke van der Hoeven <hungerandthirst@gmail.com>
2013-11-01 15:32:26 -0400

    Use time now if no time found

--- lib/tfs_graph/store_helpers.rb
+++ lib/tfs_graph/store_helpers.rb
@@ -11,18 +11,9 @@ module TFSGraph
 
     def last_updated_on
       date = Related.redis.get(UPDATED_KEY)
-      return last_updated_from_project unless date
+      return Time.now unless date
 
       Time.parse(date).localtime
-    end
-
-    def last_updated_from_project
-      last_changes = ProjectStore.all_cached.map {|p| p.last_change }.flatten.compact
-      last_time = last_changes.max {|a,b| a.created <=> b.created }.created
-
-      # cache if we're looking up for the first time
-      mark_as_updated last_time.dup
-      last_time
     end
   end
 end
