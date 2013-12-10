58a1a17b9f0046ea006d73cd043efccc883b3596
Luke van der Hoeven <hungerandthirst@gmail.com>
2013-12-10 10:31:00 -0500

    per project populator

--- lib/tfs_graph/populators.rb
+++ lib/tfs_graph/populators.rb
@@ -3,6 +3,7 @@ require_relative 'populators/utilities'
 require_relative 'populators/everything'
 require_relative 'populators/since_last'
 require_relative 'populators/since_date'
+require_relative 'populators/for_project'
 
 module TFSGraph
   module Populators
---
+++ lib/tfs_graph/populators/for_project.rb
@@ -0,0 +1,18 @@
+module TFSGraph
+  module Populators
+    class ForProject
+      include Utilities
+
+      def initialize(project)
+        @project = project
+      end
+
+      def populate
+        branches = collect_branches(@project)
+        branches.map {|branch| collect_changesets(branch) }
+
+        branches.each {|branch| collect_merges(branch) }
+      end
+    end
+  end
+end
