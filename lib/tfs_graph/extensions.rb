42f49da9fd8211584f1bf2d027fdca7d37d6efa5
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-01-30 17:45:03 -0500

    base name helper for classes using ext

--- lib/tfs_graph/extensions.rb
+++ lib/tfs_graph/extensions.rb
@@ -1,15 +1,28 @@
 require 'active_support/inflector'
+require 'active_support/concern'
 
 module TFSGraph
   module Extensions
-    private
+    extend ActiveSupport::Concern
 
+    module ClassMethods
+      private
+      def base_class_name
+        name.demodulize.downcase
+      end
+    end
+
+    private
     def add_behavior(repo, additions)
       repo.extend additions
     end
 
     def constantize(string)
       string.constantize
+    end
+
+    def base_class_name
+      self.class.base_class_name
     end
   end
 end
