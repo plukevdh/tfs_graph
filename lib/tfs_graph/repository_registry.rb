555328c83aa1a9003de8f586dda204aa94890568
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-01-30 17:52:17 -0500

    make the register a singleton

--- lib/tfs_graph/repository_registry.rb
+++ lib/tfs_graph/repository_registry.rb
@@ -1,25 +1,51 @@
+require 'singleton'
 require 'tfs_graph/extensions'
 
 module TFSGraph
   class RepositoryRegistry
     include Extensions
+    include Singleton
 
     TYPES = %w(branch changeset project)
 
-    def initialize(repo)
-      @base_repo = repo
+    def self.register
+      # assume re-registering means we want to clear existing repos
+      instance.reset
+      yield instance if block_given?
+
+      instance
+    end
+
+    def initialize
+      reset
+    end
+
+    def reset
+      @base_repo = nil
+
+      TYPES.each do |type|
+        instance_variable_set repo_memo(type), nil
+      end
+    end
+    def type(type)
+      @base_repo = type
     end
 
     TYPES.each do |type|
       define_method "#{type}_repository" do
-        existing = instance_variable_get("@#{type}_repo")
+        existing = instance_variable_get repo_memo(type)
         return existing unless existing.nil?
 
         repo = @base_repo.new constantize("TFSGraph::#{type.capitalize}")
 
-        instance_variable_set("@#{type}_repo", repo)
+        instance_variable_set(repo_memo(type), repo)
         repo
       end
+    end
+
+    private
+    def repo_memo(type)
+      "@#{type}_repo"
     end
   end
 end
