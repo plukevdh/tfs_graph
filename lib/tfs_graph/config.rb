03c21d9d4f7ed23f882e393f4573010b78911da5
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-02-11 15:05:13 -0500

    don't use ruby 2.1 syntax yet
    not available for jruby yet

--- lib/tfs_graph/config.rb
+++ lib/tfs_graph/config.rb
@@ -2,7 +2,8 @@ module TFSGraph
   class Config
     attr_accessor :tfs, :graph
 
-    def graph(repo_type:, server_path:)
+    def graph(repo_type: nil, server_path: nil)
+      raise ArgumentError unless (repo_type && server_path)
       RepositoryRegistry.register do |r|
         r.type repo_type
         r.server server_path
