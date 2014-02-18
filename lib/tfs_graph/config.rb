0bdbddafcfaecad0653b3457a80e8f7fc8ad037a
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-02-18 18:09:03 -0500

    allow redis config to be passed

--- lib/tfs_graph/config.rb
+++ lib/tfs_graph/config.rb
@@ -2,6 +2,12 @@ module TFSGraph
   class Config
     attr_accessor :tfs, :graph
 
+    def redis(url: url, namespace: namespace)
+      ServerRegistry.register do |r|
+        r.redis url: url, namespace: namespace
+      end
+    end
+
     def graph(repo_type: nil)
       raise ArgumentError unless repo_type
       RepositoryRegistry.register do |r|
