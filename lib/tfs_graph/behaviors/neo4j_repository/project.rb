927e90d6897eb9f4e1fca3af353b6e6fe3dc8b81
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-03-05 11:36:56 -0500

    dry the query craziness

--- lib/tfs_graph/behaviors/neo4j_repository/project.rb
+++ lib/tfs_graph/behaviors/neo4j_repository/project.rb
@@ -2,6 +2,9 @@ module TFSGraph
   class Behaviors
     class Neo4jRepository
       module Project
+        ROOT_BRANCH_QUERY = "MATCH (p:project {name: {project}})-[:branches]->(b:branch) WHERE (b.original_path = {path} OR b.root = {path})"
+        ACTIVITY_QUERY = "MATCH (a:project)-[:branches]->(b:branch)-[:changesets]->(c:changeset) where a.name = {name}"
+
         def create(args)
           obj = super
           relate :projects, root, obj.db_object
@@ -20,51 +23,53 @@ module TFSGraph
           rebuild project
         end
 
-        def branches_for_root(project, branch, archived=false, hidden=false)
-          branches = session.query "#{root_branch_query(archived, hidden)} RETURN b as `branch`, ID(b) as `neo_id`",
+        def branches_for_root(project, branch)
+          branches = session.query "#{ROOT_BRANCH_QUERY} RETURN b as `branch`, ID(b) as `neo_id`",
             project: project.name,
             path: branch.path
 
-          branches.each_slice(2).map do |data,id|
-            RepositoryRegistry.branch_repository.rebuild_from_query data[:branch]['data'], id[:neo_id]
-          end
+          rebuild_from_query RepositoryRegistry.branch_repository, branches, :branch
         end
 
-        def changesets_for_root(project, branch, archived=false, hidden=false)
-          changesets = session.query "#{root_branch_query(archived, hidden)} MATCH b-[:changesets]->(c:changeset) RETURN c AS `changeset`, ID(b) as `neo_id` ORDER BY c.id",
+        def active_branches_for_root(project, branch)
+          branches = session.query "#{ROOT_BRANCH_QUERY} AND b.archived = 'false' AND b.hidden = 'false' RETURN b as `branch`, ID(b) as `neo_id`",
             project: project.name,
             path: branch.path
 
-          changesets.each_slice(2).map do |data,id|
-            RepositoryRegistry.changeset_repository.rebuild_from_query data[:changeset]['data'], id[:neo_id]
-          end
+          rebuild_from_query RepositoryRegistry.branch_repository, branches, :branch
+        end
+
+        def changesets_for_root(project, branch)
+          changesets = session.query "#{ROOT_BRANCH_QUERY} MATCH b-[:changesets]->(c:changeset) RETURN c AS `changeset`, ID(b) as `neo_id` ORDER BY c.id",
+            project: project.name,
+            path: branch.path
+
+          rebuild_from_query RepositoryRegistry.changeset_repository, changesets, :changeset
         end
 
         def activity(project)
-          changesets = session.query "MATCH (a)-[:branches]->(b)-[:changesets]->(c) where a.name = {name} RETURN c as `changeset`, ID(b) as `neo_id`",
+          changesets = session.query "#{ACTIVITY_QUERY} RETURN c as `changeset`, ID(b) as `neo_id`",
             name: project.name
 
-          changesets.each_slice(2).map do |data,id|
-            RepositoryRegistry.changeset_repository.rebuild_from_query data[:changeset]['data'], id[:neo_id]
-          end
+          rebuild_from_query RepositoryRegistry.changeset_repository, changesets, :changeset
         end
 
         def activity_by_date(project, time)
-          changesets = session.query "MATCH (a)-[:branches]->(b)-[:changesets]->(c) WHERE a.name = {name} AND c.created >= {time} RETURN c as `changeset`, ID(b) as `neo_id`",
+          changesets = session.query "#{ACTIVITY_QUERY} AND c.created >= {time} RETURN c as `changeset`, ID(b) as `neo_id`",
             { name: project.name, time: time.utc.to_i }
 
-          changesets.each_slice(2).map do |data,id|
-            RepositoryRegistry.changeset_repository.rebuild_from_query data[:changeset]['data'], id[:neo_id]
-          end
+          rebuild_from_query RepositoryRegistry.changeset_repository, changesets, :changeset
         end
 
         private
-        def fetch_existing_record(obj)
-          find_by_name(obj.name).db_object
+        def rebuild_from_query(repo, data, key)
+          data.each_slice(2).map do |data,id|
+            repo.rebuild_from_query data[key]['data'], id[:neo_id]
+          end
         end
 
-        def root_branch_query(include_archived=true, include_hidden=true)
-          "MATCH (p:project {name: {project}})-[:branches]->(b:branch {archived: '#{include_archived}', hidden: '#{include_hidden}'}) WHERE b.original_path = {path} OR b.root = {path}"
+        def fetch_existing_record(obj)
+          find_by_name(obj.name).db_object
         end
       end
     end
